635d520f3998ef2e08e3f6ff03c1401607cbe02c
Ibukun Ekunwe <ekunweibukun159357@gmail.com>
2024-07-28 16:56:51 +0100

    add eslint to lsp config

--- init.lua
+++ init.lua
@@ -499,6 +499,7 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
         tsserver = {},
+        eslint = {},
         --
 
         lua_ls = {
