d0afa1041de88bf770358328b88172606dfa26ed
Ibukun Ekunwe <ekunweibukun159357@gmail.com>
2024-08-19 19:17:47 +0100

    use keymap instead of CursorHold event for opening diagnostic float

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -11,12 +11,5 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
-vim.api.nvim_create_autocmd('CursorHold', {
-  desc = 'Automatically open diagnostic float on cursor hold',
-  callback = function()
-    vim.diagnostic.open_float(nil, { focus = false })
-  end,
-})
-
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,6 +5,9 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>d', function ()
+    vim.diagnostic.open_float(nil, { focus = false })
+end, { desc = 'Open diagnostic float' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
