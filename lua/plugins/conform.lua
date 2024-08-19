f3146e5b9fe7e22b5cfe74d86e7110b7ca173146
Ibukun Ekunwe <ekunweibukun159357@gmail.com>
2024-08-19 19:17:03 +0100

    fix issue with document formatting multiple times

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -23,6 +23,7 @@ return {
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         -- javascript = { { "prettierd", "prettier" } },
+       typescript = { "prettierd", "prettier", stop_after_first = true },
       },
     },
   },
