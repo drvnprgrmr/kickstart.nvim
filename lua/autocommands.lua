aefee4ca29da9f2c657fa6d57df962347e9fce39
Ibukun Ekunwe <ekunweibukun159357@gmail.com>
2024-11-06 12:56:15 +0100

    auto save view (particularly folds)

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -11,5 +11,21 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+
+-- auto save view
+local auto_save_view = vim.api.nvim_create_augroup('AutoSaveView', { clear = true })
+vim.api.nvim_create_autocmd('BufWinLeave', {
+  pattern = '?*.*', -- files must have extension in name and should exist
+  desc = "Save 'view' when leaving the file.",
+  command = 'mkview',
+  group = auto_save_view,
+})
+vim.api.nvim_create_autocmd('BufWinEnter', {
+  pattern = '?*.*', -- files must have extension in name and should exist
+  desc = "Load 'view' when opening a file.",
+  command = 'silent! loadview',
+  group = auto_save_view,
+})
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
