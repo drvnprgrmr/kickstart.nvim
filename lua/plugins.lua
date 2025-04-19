aedda3ab61be570cc65568bc2b0a9589231706c0
Ibukun Ekunwe <ekunweibukun159357@gmail.com>
2025-04-19 06:06:57 +0100

    add which-key plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -41,6 +41,7 @@ require('lazy').setup({
   -- you do for a plugin at the top level, you can do for a dependency.
   --
   -- Use the `dependencies` key to specify the dependencies of a particular plugin
+  require 'plugins.which-key',
 
   require 'plugins.telescope',
   require 'plugins.lspconfig',
@@ -53,12 +54,6 @@ require('lazy').setup({
   require 'plugins.lint',
   require 'plugins.autopairs',
 
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
-  --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
